112335f4e7495bfdfcae669e500466c1b8652433
Brandon <nbr4nd0n2005@gmail.com>
2025-08-21 14:35:57 +0700

    Moved dfx.json to the root of the folder to deploy in the ICP

---
+++ .dfx/local/canisters/learnsphere_backend/service.did.d.ts
@@ -0,0 +1,30 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+import type { IDL } from '@dfinity/candid';
+
+export interface Quest {
+  'id' : bigint,
+  'title' : string,
+  'rewardAmount' : bigint,
+  'link' : string,
+  'description' : string,
+  'prerequisite' : [] | [bigint],
+  'category' : string,
+}
+export interface UserStats {
+  'experience' : bigint,
+  'questsCompleted' : Array<bigint>,
+  'tokens' : bigint,
+}
+export interface _SERVICE {
+  'completeQuest' : ActorMethod<[bigint], string>,
+  'getAllQuests' : ActorMethod<[], Array<Quest>>,
+  'getAllQuestsWithStatus' : ActorMethod<[], Array<[Quest, string]>>,
+  'getNextQuest' : ActorMethod<[], [] | [Quest]>,
+  'getQuestById' : ActorMethod<[bigint], [] | [Quest]>,
+  'getQuestCount' : ActorMethod<[], bigint>,
+  'getUserStats' : ActorMethod<[], UserStats>,
+  'greet' : ActorMethod<[string], string>,
+}
+export declare const idlFactory: IDL.InterfaceFactory;
+export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
